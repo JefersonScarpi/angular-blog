d1ad14b19b54ea890d0eaee08e4efd04902258a4
Jeferson Scarpi <jefersonscarpi@gmail.com>
2023-07-08 15:05:26 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo":"aaaa",
+        "title":"saiu novo filme",
+        "description":"ola",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=506&aspectRatio=2.00&format=jpeg"
+    },
+    {
+        "id":2,
+        "photo":"aaaa",
+        "title":"saiu novo filme",
+        "description":"ola",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+    {
+        "id":3,
+        "photo":"aaaa",
+        "title":"saiu novo filme",
+        "description":"ola",
+        "photoCover":"https://imageio.forbes.com/blogs-images/scottmendelson/files/2019/02/captain-marvel-international-poster-top-1200x675.jpg?format=jpg&width=960"
+    },
+    {
+        "id":4,
+        "photo":"aaaa",
+        "title":"saiu novo filme",
+        "description":"ola",
+        "photoCover":"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2018%2F03%2Fsab1035_comp_v042-1153-2000.jpg&q=60"
+    }
+
+
+]
